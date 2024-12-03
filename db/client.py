cf76262b7e5cf074869f54c9b052ee0daf56bc39
Rodrigo Oyarzun <89427203+rodrigooya@users.noreply.github.com>
2024-12-03 19:08:37 -0300

    requirements commit

--- db/client.py
+++ db/client.py
@@ -1,7 +1,11 @@
+import os
 from pymongo import MongoClient
-from dotenv import dotenv_values
+#from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-config = dotenv_values(".env") 
-
-db_client = MongoClient(config["ATLAS_URI"])
+load_dotenv()
+#config = dotenv_values(".env")
+ATLAS_URI = os.getenv("ATLAS_URI")     
+db_client = MongoClient(ATLAS_URI)
+#db_client = MongoClient(config["ATLAS_URI"])
 
